edad346d1ae0106e3700775b1f1606eb7babc6f2
Zahra Foroozesh <zahraforoozesh84@gmail.com>
2025-07-12 18:06:46 -0700

    i think chat listview compeleted

---
+++ src/main/java/org/backrooms/backroom_messenger/ChatPageController.java
@@ -0,0 +1,15 @@
+package org.backrooms.backroom_messenger;
+
+import org.backrooms.backroom_messenger.entity.Chat;
+import org.backrooms.backroom_messenger.entity.User;
+
+public class ChatPageController {
+
+    private User user = null;
+
+    private Chat chat = null;
+
+    public void setChatAndUser(Chat chat, User user) {
+
+    }
+}
