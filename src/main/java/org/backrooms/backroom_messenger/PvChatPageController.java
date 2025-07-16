75fe3e9eee6e4e332c26175e88dd8e9c3eb5a903
Zahra Foroozesh <zahraforoozesh84@gmail.com>
2025-07-16 01:38:33 -0700

    working on pv chat page

--- src/main/java/org/backrooms/backroom_messenger/PvChatPageController.java
+++ src/main/java/org/backrooms/backroom_messenger/PvChatPageController.java
@@ -1,17 +1,19 @@
 package org.backrooms.backroom_messenger;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.backrooms.backroom_messenger.client.Client;
 import org.backrooms.backroom_messenger.entity.Chat;
 import org.backrooms.backroom_messenger.entity.Message;
 import org.backrooms.backroom_messenger.entity.User;
-import org.w3c.dom.Text;
 
 import java.io.IOException;
 
@@ -19,21 +21,36 @@ public class PvChatPageController {
 
     private User user = null;
     private static Chat chat = null;
+    private ObservableList<Message> messages = FXCollections.observableArrayList();
 
     @FXML
     private TextField Message;
+    @FXML
+    private ListView<Message> senderListView;
+    @FXML
+    private ListView<Message> receiverListView;
 
     @FXML
 
     public void setChatAndUser(Chat chat, User user) {
-        this.chat = chat;
+        PvChatPageController.chat = chat;
         this.user = user;
+        messages.clear();
+        messages.setAll(chat.getMessage());
+
+        senderListView.setItems(messages);
+        receiverListView.setItems(messages);
+
+        //setupSenderCellFactory();
+        //setupReceiverCellFactory();
     }
 
 
     public void sendMessage(ActionEvent event) {
         String message = Message.getText();
         chat.getMessage().add(Client.sendMessage(message, chat));
+        messages.setAll(chat.getMessage());
+        Message.clear();
     }
 
 
