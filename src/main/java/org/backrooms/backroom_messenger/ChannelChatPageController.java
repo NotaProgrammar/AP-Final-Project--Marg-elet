909fce837168200c973bed25aa50d3af1cd5ba01
Zahra Foroozesh <zahraforoozesh84@gmail.com>
2025-07-23 21:13:36 -0700

    creat channel is compeleted

--- src/main/java/org/backrooms/backroom_messenger/ChannelChatPageController.java
+++ src/main/java/org/backrooms/backroom_messenger/ChannelChatPageController.java
@@ -2,12 +2,18 @@ package org.backrooms.backroom_messenger;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import org.backrooms.backroom_messenger.entity.Channel;
 import org.backrooms.backroom_messenger.entity.Chat;
 import org.backrooms.backroom_messenger.entity.User;
+
+import java.io.IOException;
 
 public class ChannelChatPageController {
 
@@ -18,16 +24,27 @@ public class ChannelChatPageController {
     private Label joinNotification;
 
 
+    public void goBack(ActionEvent event) throws IOException {
+        FXMLLoader displayLoader = new FXMLLoader(BackRoomMessengerApplication.class.getResource("MainDisplay.fxml"));
+        Scene scene = new Scene(displayLoader.load(), 560, 350);
+        MainDisplayController mdc  = displayLoader.getController();
+        mdc.setUser(this.user);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
     public void joinChannel(ActionEvent event) {
         //todo : boolean alreadyJoined = یه تابع توی بک باشه که یوزر رو بهش بدم و بگرده اگر یوزر توی چنل بوده
-        boolean alreadyJoined = false;
+        boolean alreadyJoined = User.isSubed(chat);
 
         if(alreadyJoined == false) {
             joinNotification.setTextFill(Color.GREEN);
             joinNotification.setText("you joined the Channel");
             //todo : get the created chat for user to add in channel
             //todo : همون که وقتی جوین داد تو چنل به لیست چت هاش اضافه بشه
-            User.getChats().add()
+
         }
     }
 
--- src/main/java/org/backrooms/backroom_messenger/CreatChannelPageController.java
+++ src/main/java/org/backrooms/backroom_messenger/CreatChannelPageController.java
@@ -2,7 +2,13 @@ package org.backrooms.backroom_messenger;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.backrooms.backroom_messenger.client.Client;
 import org.backrooms.backroom_messenger.entity.Channel;
 import org.backrooms.backroom_messenger.entity.Chat;
 import org.backrooms.backroom_messenger.entity.User;
@@ -13,16 +19,45 @@ public class CreatChannelPageController {
 
     private User user = null;
     private Channel channel = null;
+    boolean publicChannel;
 
     @FXML
     private TextField description;
     @FXML
     private TextField name;
+    @FXML
+    private RadioButton publicity;
+    @FXML
+    private RadioButton privateChannel;
 
 
     public void creat(ActionEvent event){
         String channelName = name.getText();
         String channelDescription = description.getText();
+        channel = Client.createChannel(channelName, channelDescription, publicChannel);
+        user.getChats().add(channel);
+
+    }
+
+
+    public void setPublicityOfChannel(ActionEvent event){
+        if(publicity.isSelected()){
+            publicChannel = true;
+        }
+        else if(privateChannel.isSelected()){
+            publicChannel = false;
+        }
+    }
+
+
+    public void goToChannelPage(ActionEvent event) throws IOException {
+        FXMLLoader channelLoader = new FXMLLoader(BackRoomMessengerApplication.class.getResource("ChannelChatPage.fxml"));
+        Scene scene = new Scene(channelLoader.load(), 900, 550);
+        ChannelChatPageController ccpc = channelLoader.getController();
+        ccpc.setUserAndChat(user, channel);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
 
